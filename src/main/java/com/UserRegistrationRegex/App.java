3836641f827a5c79009b93e43530a8c3c7495b60
AZahi93 <zahedanaguru@gmail.com>
2022-10-01 20:16:10 +0530

    commit

---
+++ src/main/java/com/UserRegistrationRegex/App.java
@@ -0,0 +1,13 @@
+package com.UserRegistrationRegex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
